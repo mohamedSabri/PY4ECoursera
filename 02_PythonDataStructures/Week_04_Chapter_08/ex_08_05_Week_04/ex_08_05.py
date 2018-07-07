ff886ff9b969afaf89bf477623d79d9867ff5c7a
mohamed sabri <mohamedsabri.wo@gmail.com>
2018-07-08 00:30:24 +0200

    solution for exercise 08_05 for Week 4 Chapter 8

---
+++ 02_PythonDataStructures/Week_04_Chapter_08/ex_08_05_Week_04/ex_08_05.py
@@ -0,0 +1,16 @@
+fname = input("Enter file name: ")
+if len(fname) < 1:
+    fname = "mbox-short.txt"
+
+fhand = open(fname)
+count = 0
+for line in fhand:
+    if not line.startswith("From "):
+        continue
+    count += 1
+    line = line.rstrip()
+    words = line.split()
+    print (words[1])
+
+
+print("There were", count, "lines in the file with From as the first word")
