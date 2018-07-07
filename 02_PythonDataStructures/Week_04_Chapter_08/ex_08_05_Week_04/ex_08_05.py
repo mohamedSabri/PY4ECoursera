dc3ed92c8d12afe68bee9fe6f9ca8a1dc6105f2e
mohamed sabri <mohamedsabri.wo@gmail.com>
2018-07-08 00:33:29 +0200

    just check indention for if statements

--- 02_PythonDataStructures/Week_04_Chapter_08/ex_08_05_Week_04/ex_08_05.py
+++ 02_PythonDataStructures/Week_04_Chapter_08/ex_08_05_Week_04/ex_08_05.py
@@ -1,12 +1,10 @@
 fname = input("Enter file name: ")
-if len(fname) < 1:
-    fname = "mbox-short.txt"
+if len(fname) < 1:fname = "mbox-short.txt"
 
 fhand = open(fname)
 count = 0
 for line in fhand:
-    if not line.startswith("From "):
-        continue
+    if not line.startswith("From "): continue
     count += 1
     line = line.rstrip()
     words = line.split()
